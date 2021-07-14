67cd7dea4f2631c9a74d4a5cc1e8cc58f3758089
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-07-14 14:40:35 -0400

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m HunterAlpha
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE HunterAlpha
+:: Enables virtual env mode and then starts HunterAlpha
+env\scripts\activate.bat && py -m HunterAlpha
