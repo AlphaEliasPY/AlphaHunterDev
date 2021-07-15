303e5782d24277fb8790cdee10393405b2fd5a29
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-07-15 13:47:40 -0400

    Update sample_config.py

--- HunterAlpha/sample_config.py
+++ HunterAlpha/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 123456789  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 682039324  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "awoo"
     SUPPORT_CHAT = "awoo"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1234567890123
+        -1001478416290
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1234567890123
+        -1001478416290
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     ALLOW_CHATS = True
 
