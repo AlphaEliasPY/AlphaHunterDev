5e8b7bf0bef93ba17c82940309d4998b5b741d91
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-07-16 12:55:46 -0400

    Update dbcleanup.py

--- HunterAlpha/modules/dbcleanup.py
+++ HunterAlpha/modules/dbcleanup.py
@@ -25,7 +25,7 @@ def get_invalid_chats(update: Update, context: CallbackContext, remove: bool = F
     for chat in chats:
 
         if ((100 * chats.index(chat)) / len(chats)) > progress:
-            progress_bar = f"{progress}% completed in getting invalid chats."
+            progress_bar = f"{progress}% completado en la obtención de chats inválidos."
             if progress_message:
                 try:
                     bot.editMessageText(
@@ -91,16 +91,16 @@ def get_invalid_gban(update: Update, context: CallbackContext, remove: bool = Fa
 def dbcleanup(update: Update, context: CallbackContext):
     msg = update.effective_message
 
-    msg.reply_text("Getting invalid chat count ...")
+    msg.reply_text("Obteniendo un recuento de chat no válido ...")
     invalid_chat_count = get_invalid_chats(update, context)
 
-    msg.reply_text("Getting invalid gbanned count ...")
+    msg.reply_text("Obtener un recuento de gbanned no válido ...")
     invalid_gban_count = get_invalid_gban(update, context)
 
-    reply = f"Total invalid chats - {invalid_chat_count}\n"
-    reply += f"Total invalid gbanned users - {invalid_gban_count}"
+    reply = f"Total de chats no válidos - {invalid_chat_count}\n"
+    reply += f"Total de usuarios de gbanned no válidos - {invalid_gban_count}"
 
-    buttons = [[InlineKeyboardButton("Cleanup DB", callback_data="db_cleanup")]]
+    buttons = [[InlineKeyboardButton("Limpieza de Base datos ", callback_data="db_cleanup")]]
 
     update.effective_message.reply_text(
         reply, reply_markup=InlineKeyboardMarkup(buttons)
@@ -120,22 +120,22 @@ def callback_button(update: Update, context: CallbackContext):
 
     if query_type == "db_leave_chat":
         if query.from_user.id in admin_list:
-            bot.editMessageText("Leaving chats ...", chat_id, message.message_id)
+            bot.editMessageText("Dejando chats...", chat_id, message.message_id)
             chat_count = get_muted_chats(update, context, True)
             bot.sendMessage(chat_id, f"Left {chat_count} chats.")
         else:
-            query.answer("You are not allowed to use this.")
+            query.answer("No se le permite usar esto.")
     elif query_type == "db_cleanup":
         if query.from_user.id in admin_list:
-            bot.editMessageText("Cleaning up DB ...", chat_id, message.message_id)
+            bot.editMessageText("Limpieza de Base datos ...", chat_id, message.message_id)
             invalid_chat_count = get_invalid_chats(update, context, True)
             invalid_gban_count = get_invalid_gban(update, context, True)
-            reply = "Cleaned up {} chats and {} gbanned users from db.".format(
+            reply = "Limpiado {} chats y {} usuarios gbanned de db.".format(
                 invalid_chat_count, invalid_gban_count
             )
             bot.sendMessage(chat_id, reply)
         else:
-            query.answer("You are not allowed to use this.")
+            query.answer("No se le permite usar esto.")
 
 
 DB_CLEANUP_HANDLER = CommandHandler("dbcleanup", dbcleanup, run_async=True)
@@ -144,5 +144,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*", run_asyn
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "DB Cleanup"
+__mod_name__ = "Limpieza de base de datos"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
