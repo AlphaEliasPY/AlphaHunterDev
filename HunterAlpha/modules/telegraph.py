63b217ca5b1d1d6337f70938377377a48e28afa9
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-08-01 20:34:23 -0400

    Update telegraph.py

--- HunterAlpha/modules/telegraph.py
+++ HunterAlpha/modules/telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 HunterAlpha = "HunterAlpha"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=DarkGod14)
+r = telegraph.create_account(short_name=HunterAlpha)
 auth_url = r["auth_url"]
 
 
