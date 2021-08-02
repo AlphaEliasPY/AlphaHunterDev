89b46aa3ecd886307b5f9676e6160cd842be8002
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-08-01 20:43:39 -0400

    Update purge.py

--- HunterAlpha/modules/purge.py
+++ HunterAlpha/modules/purge.py
@@ -25,16 +25,16 @@ async def purge_messages(event):
     if not await user_is_admin(
         user_id=event.sender_id, message=event
     ) and event.from_id not in [1087968824]:
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Solo los administradores pueden usar este comando.")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("Parece que no puedo purgar el mensaje")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply("Responda a un mensaje para seleccionar desde dónde comenzar a purgar.")
         return
     messages = []
     message_id = reply_msg.id
@@ -52,7 +52,7 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    text = f"Purgado con éxito en {time_:0.2f} Segundo(s)"
     delmsg = await event.respond(text, parse_mode="markdown")
     
     cleartime = get_clearcmd(event.chat_id, "purge")
@@ -70,16 +70,16 @@ async def delete_messages(event):
     if not await user_is_admin(
         user_id=event.sender_id, message=event
     ) and event.from_id not in [1087968824]:
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Solo los administradores pueden usar este comando.")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("Parece que no puedo eliminar esto?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("Whadya quiere eliminar?")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
@@ -93,4 +93,4 @@ __help__ = """
  • `/purge <integer X>`: deletes the replied message, and X messages following it if replied to a message.
 """
 
-__mod_name__ = "Purges"
+__mod_name__ = "Purgas"
