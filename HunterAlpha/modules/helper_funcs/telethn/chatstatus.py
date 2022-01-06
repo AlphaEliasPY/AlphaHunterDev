95556f2a6d38430e7a43b221769e5e78e9991088
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2022-01-06 16:16:34 -0300

    Update chatstatus.py

--- HunterAlpha/modules/helper_funcs/telethn/chatstatus.py
+++ HunterAlpha/modules/helper_funcs/telethn/chatstatus.py
@@ -44,7 +44,7 @@ async def is_user_admin(user_id: int, chat_id):
 
 async def HunterAlpha_is_admin(chat_id: int):
     status = False
-    DarkGod14Bot = await telethn.get_me()
+    HunterAlpha = await telethn.get_me()
     async for user in telethn.iter_participants(
         chat_id, filter=ChannelParticipantsAdmins
     ):
