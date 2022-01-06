1efca3a5e44fde132a229ccd4e1a6d894487a7de
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2022-01-06 15:59:45 -0300

    Update chat_status.py

--- HunterAlpha/modules/helper_funcs/chat_status.py
+++ HunterAlpha/modules/helper_funcs/chat_status.py
@@ -16,6 +16,7 @@ from HunterAlpha import (
 from telegram import Chat, ChatMember, ParseMode, Update
 from telegram.ext import CallbackContext
 
+# stores admemes in memory for 10 min.
 ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
 THREAD_LOCK = RLock()
 
@@ -38,14 +39,18 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         or user_id in SUDO_USERS
         or user_id in DEV_USERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
-    ):
+        or user_id in [1087968824]
+    ):  # Count telegram and Group Anonymous as admin
         return True
     if not member:
         with THREAD_LOCK:
+            # try to fetch from cache first.
             try:
                 return user_id in ADMIN_CACHE[chat.id]
             except KeyError:
+                # keyerror happend means cache is deleted,
+                # so query bot api again and return user status
+                # while saving it in cache for future useage...
                 chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
                 admin_list = [x.user.id for x in chat_admins]
                 ADMIN_CACHE[chat.id] = admin_list
@@ -76,8 +81,8 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
         or user_id in DEV_USERS
         or user_id in WHITELIST_USERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
-    ):
+        or user_id in [1087968824]
+    ):  # Count telegram and Group Anonymous as admin
         return True
 
     if not member:
@@ -382,7 +387,7 @@ def user_can_ban(func):
         if (
             not (member.can_restrict_members or member.status == "creator")
             and user not in SUDO_USERS
-            and user not in [777000, 1087968824]
+            and user not in [1087968824]
         ):
             update.effective_message.reply_text(
                 "Sorry son, but you're not worthy to wield the banhammer."
@@ -420,6 +425,7 @@ def connection_status(func):
     return connected_status
 
 
+# Workaround for circular import with connection.py
 from HunterAlpha.modules import connection
 
 connected = connection.connected
